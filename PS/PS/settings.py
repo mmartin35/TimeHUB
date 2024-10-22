d4d2099fc00a9528cbc3a1b823a68baca9470803
matthieu_admin <matthieu.martin@dlh.lu>
2024-10-22 15:50:26 +0200

    updated settings.py

--- PS/PS/settings.py
+++ PS/PS/settings.py
@@ -52,9 +52,11 @@ TEMPLATES = [
     },
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 WSGI_APPLICATION = 'PS.wsgi.application'
 
