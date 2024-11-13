e3056b21c416f4a3af3057480d133737a91a1a37
user1 <matthieu1.martin@epitech.eu>
2024-11-13 16:56:58 +0100

    settings updated

--- PS/PS/settings.py
+++ PS/PS/settings.py
@@ -10,7 +10,56 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 ALLOWED_HOSTS = ['*']
-#CSRF_TRUSTED_ORIGINS = ['https://10.253.48.166', 'https://timehub.emp.dlh.lu']
+CSRF_TRUSTED_ORIGINS = ['https://10.253.48.166', 'https://timehub.emp.dlh.lu']
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'intern',
+    'pointer',
+    'planning',
+    'admin_panel',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'PS.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+linux: ~/Code/pointing_system/PS [main|✔] ❯❯❯ s 
+zsh: command not found: s
+linux: ~/Code/pointing_system/PS [main|✔] ❯❯❯ git switch prod    
+Switched to branch 'prod'
+Your branch is up to date with 'origin/prod'.
+linux: ~/Code/pointing_system/PS [prod|✔] ❯❯❯ ccat PS/settings.py 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-0ce%20&$+$hjorddu(5h9*ec@v_t2c&5ypg*o)s+iv0r0_vr(c'
+DEBUG = True
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://10.253.48.166', 'https://timehub.emp.dlh.lu']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -92,5 +141,5 @@ CLIENT_ID = '8daf41b5-4a5a-44cb-880f-1733b9865ab2'
 CLIENT_SECRET = '-AD8Q~qzVCDnrgLbJpm.qNTtQLXQmdlWfDAska.8'
 #REDIRECT_URI = 'https%3A%2F%2F10.253.48.166%2Faccount%2F'
 #ABSOLUTE_URI = 'https://10.253.48.166/account/'
-REDIRECT_URI = 'http%3A%2F%2Flocalhost%3A8000%2Faccount%2F'
-ABSOLUTE_URI = 'http://localhost:8000/account/'
+REDIRECT_URI = 'https%3A%2F%2Ftimehub.emp.dlh.lu%2Faccount%2F'
+ABSOLUTE_URI = 'https://timehub.emp.dlh.lu/account/'
